7d8fec7ac9e7d2286777e82ed37d1d2ab3e55c48
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-02-23 10:27:23 +0100

    Create SetAzureADUser-AlternateEmail-bulk.ps1

---
+++ SetAzureADUser-AlternateEmail-bulk.ps1
@@ -0,0 +1,25 @@
+#>
+In the need to add alternateEmailAddress in bulk in Azure AD? This script will help you out!
+
+
+CSV format:
+userPrincipalName,alternateEmailAddress
+user1@domain.com,user1@otherdomain.com
+user2@domain.com,user2@otherdomain.com
+
+userPrincipalName = column A in the CSV file
+alternateEmailAddress = column B in the CSV file
+
+#>
+
+
+$Members = Import-Csv -Path "C:\temp\users1.csv" -Delimiter “,”
+ 
+foreach ($Member in $Members){
+    Set-AzureADUser -ObjectId $Member.userPrincipalName -OtherMails $Member.alternateEmailAddress
+} 
+
+#>
+userPrincipalName = column A in the CSV file
+alternateEmailAddress = column B in the CSV file
+#>
