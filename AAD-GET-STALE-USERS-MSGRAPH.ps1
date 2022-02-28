5212e660fd4beea5df393d1bf863deb53f4ef598
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-02-28 17:54:51 +0100

    Update AAD-GET-STALE-USERS-MSGRAPH.ps1

--- AAD-GET-STALE-USERS-MSGRAPH.ps1
+++ AAD-GET-STALE-USERS-MSGRAPH.ps1
@@ -2,20 +2,21 @@
 1: First install Graph with: Install-Module Microsoft.Graph -Scope AllUsers
 2: First time run, you will need to give consent on the Graph PowerShell enterprise application in AAD and on the scope permissions below.
 #>
+
 Connect-MgGraph -Scopes 'AuditLog.Read.All','Directory.Read.All'
-Select-MgProfile beta 
 $Result=@()
-$usersUPN = Get-MgUser -All | Select-Object UserPrincipalName,ID
-foreach($user in $usersUPN)
+$Users = Get-MgUser -All | Select-Object UserPrincipalName,ID,DisplayName
+foreach($User in $Users)
 {
-    $usersignindate = Get-MgUser -UserId $user.ID -Select SignInActivity | Select-Object -ExpandProperty SignInActivity
-    $userprops = [ordered]@{
-        UserPrincipalName = $user.UserPrincipalName
-        LastSignInDateTime = $usersignindate.LastSignInDateTime
+    $LastSignInDateTime = Get-MgUser -UserId $User.ID -Select SignInActivity | Select-Object -ExpandProperty SignInActivity
+    $OutputTable = [ordered]@{
+        Displayname        =  $User.DisplayName
+        UserPrincipalName  =  $User.UserPrincipalName
+        LastSignInDateTime =  $LastSignInDateTime.LastSignInDateTime
     }
-    $userObj =  new-object -Type PSObject -Property $userprops
-    $Result += $userObj
+    $OutputTableObject = New-Object -Type PSObject -Property $OutputTable
+    $Result += $OutputTableObject
 }
-$Result | Export-Csv "C:\Temp\$((Get-Date).ToString("yyyyMMdd"))_aad-stale-users.csv" -NoTypeInformation
+$Result | Export-Csv "C:\Temp\$((Get-Date).ToString("yyyyMMdd"))_aad-stale-users6.csv" -NoTypeInformation
 
 Disconnect-Graph
