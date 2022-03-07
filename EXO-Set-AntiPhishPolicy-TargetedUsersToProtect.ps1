43d9d5857f93d3577cfc5a43ef277890b24c162d
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-03-07 11:27:35 +0100

    Create EXO-Set-AntiPhishPolicy-TargetedUsersToProtect.ps1

---
+++ EXO-Set-AntiPhishPolicy-TargetedUsersToProtect.ps1
@@ -0,0 +1,16 @@
+<# Bulk adding: senders for impersonation protection
+
+Add in bulk CSV like:
+Users
+Firstname Lastname;user1@domain.com
+Firstname Lastname;user2@domain.com
+
+ForEach will not work an is not needed, because the users needs to be set in 1 row. The input is emailaddress based, for internal or external addresses.
+
+#>
+
+#Obtain the addresses in the CSV
+$Users = (Import-CSV -Path 'C:\temp\users.csv').Users
+
+#Set the senders for impersonation protection in the AntiPhishPolicy
+Set-AntiPhishPolicy -Identity "Office365 AntiPhish Default" -TargetedUsersToProtect $Users
