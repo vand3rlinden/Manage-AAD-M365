1a09229d72d4757095f6d4b8d19904390fafca7a
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-05-18 13:07:42 +0200

    Update EXO-Get-DistributionGroup-Export-All-Members.ps1

--- EXO-Get-DistributionGroup-Export-All-Members.ps1
+++ EXO-Get-DistributionGroup-Export-All-Members.ps1
@@ -1,4 +1,7 @@
-$Groups = Get-DistributionGroup -ResultSize Unlimited #-Filter {HiddenFromAddressListsEnabled -eq $false}
+$Groups = Get-DistributionGroup -ResultSize Unlimited 
+#Scope option examples: 
+##Get-DistributionGroup -ResultSize Unlimited -Filter {HiddenFromAddressListsEnabled -eq $false}
+##Get-DistributionGroup | Where-Object {$_.DisplayName -like "*DG1*" -or $_.DisplayName -like "*MSG1*" }
 $Groups | ForEach-Object {
 $group = $_.Displayname
 $type = $_.RecipientTypeDetails
