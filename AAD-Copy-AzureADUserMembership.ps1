dcd7cfc02a57fe53593f75820073e1892bbf8ef3
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-06-02 17:29:06 +0200

    Update AAD-Copy-AzureADUserMembership.ps1

--- AAD-Copy-AzureADUserMembership.ps1
+++ AAD-Copy-AzureADUserMembership.ps1
@@ -1,10 +1,10 @@
 # Copy group membership from source user to new target user
 
 #ObjectID Source User
-$Source = "d77f0d3a-f6c7-4889-8bc1-da8993f3af4a"
+$Source = "xxxxxxxxxxxxxxxxxxxxxxx"
 
 #ObjectID Target User
-$Target  = "19720dd4-8ef8-4ea2-838e-c40d4085051b"
+$Target  = "xxxxxxxxxxxxxxxxxxxxxxx"
 
 $Groups = (Get-AzureADUserMembership -ObjectId $Source).ObjectId
 
