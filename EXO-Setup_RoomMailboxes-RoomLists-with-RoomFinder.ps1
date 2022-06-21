8926fed6bfa37c150d1ea169a8b3a70f2499f4f8
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2022-06-21 09:19:40 +0200

    Update EXO-Setup_RoomMailboxes-RoomLists-with-RoomFinder.ps1

--- EXO-Setup_RoomMailboxes-RoomLists-with-RoomFinder.ps1
+++ EXO-Setup_RoomMailboxes-RoomLists-with-RoomFinder.ps1
@@ -7,25 +7,19 @@ New-DistributionGroup -Name "RoomList" -DisplayName "RoomList" -RoomList
 #3: Add the RoomMailboxes to a RoomList
 Add-DistributionGroupMember -Identity "RoomList" -Member RoomMailbox@domain.com
 
-<#
-On this point, fill in all your rooms and place the rooms into  a room lists
+#4: Set up the metadata
+Set-Place -Identity "RoomMailboxName" -City "Delft" -Capacity '4' -Building 'RoomList Displayname'
+#Other meta data to add: AudioDeviceName,VideoDeviceName,DisplayDeviceName,IsWheelChairAccessible [$true/$false]
 
-Setting up the Room Finder can find the RoomMailboxes and RoomLists, we can do this with the Set-Place cmdlet. This cmdlet let you fill in meta data, like capacity, audio devices, video devices, which city or if wheel chair accessible. When you set all the meta data, finding rooms with Outlooks Room Finder is much easier for your end- users. Also the RoomFinder only works when you set the city meta data on your room mailboxes, I also recommend always set a capicity on your rooms. to avoid disappointment of space. 
+#If Capacity is already set, you can set the City and Building meta data of all RoomMailboxes in a RoomList
+$RoomList = (Get-DistributionGroup -Identity "RoomList@domain.com").Displayname
+$City = "Delft"
+$Rooms = (Get-DistributionGroupMember -Identity "RoomList@domain.com").Displayname
+ForEach ($Room in $Rooms){
+    Set-Place -Identity $Room -City $City -Building $RoomList
+}
+
+<# 
+NOTE: The Set-Place cmdlet comes available after replication cycle of the RoomMailbox. 
+You can get an error message on new RoomMailboxes when you use the Set-Place cmdlet. The replication cycle is 24 hours in most cases.
 #>
-
-#4: Set the city and capacity metadata
-Set-Place -Identity "RoomMailboxName" -City "Delft" -Capacity '4'
-
-<#
-After you setup the city and capacity metadata, your room mailbox and room lists will appear in the room finder. 
-It can take up to 24 hours before this is in sync with the room finder in Outlook and Outlook on the web.
-#>
-
-<#
-Finetuning the room in Room Finder. 
-Is always a good idea when the room have devices like audio or video to make this visible in the Room Finder 
-and also, when the room is wheelchair accessible or not. 
-#>
-
-#5: Finetuning the room in Room Finder
-Set-Place -Identity 'RoomMailbox1' -AudioDeviceName 'Sonos' -VideoDeviceName 'Sony Beamer' -DisplayDeviceName 'Samsung TV 55' -IsWheelChairAccessible $true
